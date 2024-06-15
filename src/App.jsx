6cab7aa8b791c641180083f22fe38b5f562c3293
privilledge <privieroyalty@gmail.com>
2024-06-15 22:12:36 +0200

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<Home />} />
+
               <Route path="shop" element={<Shop />} />
               <Route path="/" element={<PrivateRoute />}>
                 <Route path="checkout" element={<Checkout />} />
